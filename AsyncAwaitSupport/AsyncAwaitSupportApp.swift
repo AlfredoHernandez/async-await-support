9bd613e1739a2355ed553a0d162859784df9285e
Jesús Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2021-06-16 11:09:07 -0500

    Initial Commit

---
+++ AsyncAwaitSupport/AsyncAwaitSupportApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncAwaitSupportApp.swift
+//  AsyncAwaitSupport
+//
+//  Created by Jesús Alfredo Hernández Alarcón on 16/06/21.
+//
+
+import SwiftUI
+
+@main
+struct AsyncAwaitSupportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
